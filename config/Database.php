4c4ad10912e2188c0029be7e25a15a191065d570
Akbardavid <akbardavid034@gmail.com>
2023-05-28 19:13:07 +0700

    rest-api

---
+++ config/Database.php
@@ -0,0 +1,20 @@
+<?php 
+    $connection = mysqli_connect('localhost', 'root', '', 'politeknikdb');
+    // class Database{
+    //     private $host = 'localhost';
+    //     private $user = 'root';
+    //     private $password = "";
+    //     private $database = '';
+
+        
+    //     public function getConnection(){
+    //         $connection = mysqli_connect($this->host, $this->user, $this->password, $this->database);
+    //         if($connection->connect_error){
+    //             die("Eror Gagal terkoneski ke MYSQL" . $connection->connect_error);
+    //         }else{
+    //             return $connection;
+    //         }
+            
+    //     }
+    // }
+?>
